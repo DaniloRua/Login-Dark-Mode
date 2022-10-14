3d9ecd3a11b82c06fa0b0d6ba1d522e7bee4fcda
Danilopc <sr_danilo@hotmail.com>
2022-10-14 03:51:41 +0200

    last update

--- darkmode.js
+++ darkmode.js
@@ -1,10 +1,19 @@
-function darkmode(){
- console.log("Dark mode enabled ");
- darkOn();
+const body = document.querySelector('body');
+const board = document.querySelector('.board');
+const apresentation = document.querySelector('.apresentation-box');
+const loginBox = document.querySelector('.login-box');
+
+function darkmode() {
+    console.log("Dark mode enabled ");
+    darkOn();
 }
 
-function darkOn(){
+function darkOn() {
     const dark = document.querySelector(".dark-off");
     dark.classList.toggle("dark-on");
+    body.classList.toggle("body-dark");
+    board.classList.toggle("board-dark");
+    apresentation.classList.toggle("apresentation-box-dark");
+    loginBox.classList.toggle("login-box-dark");
 
 }
